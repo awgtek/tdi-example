e8cd69e80f29e77ae97d5ce9a09f90dbb97618cb
Test driven Infrastructure <awgy>
2016-03-20 19:05:05 +0000

    Adding TDI cookbooks

---
+++ cookbooks/irc/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: irc
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+user 'tdi' do
+  action :create
+  comment "Test driven Infrastructure"
+  home "/home/tdi"
+  manage_home true
+end
+
+package 'irssi' do
+ action :install
+ options '--force-yes'
+end
+
+directory "/home/tdi/.irssi" do
+ owner 'tdi'
+ group 'tdi'
+end
+
+cookbook_file "/home/tdi/.irssi/config" do
+ source "irssi-config"
+ owner 'tdi'
+ group 'tdi'
+end
